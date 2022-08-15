8761c3087d2714536f0beb50864d5b992056dfe4
BambaTHIAM <bambathiam88@gmail.com>
2022-08-15 15:57:25 +0200

    Annulation intégration menu complexe

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+/* Début integration code CSS menu circulaire => source codepen.io/mahmoud-nb */
+
+$(".toggleMenu").on('click', function(){
+    $("#mainMenu").toggleClass('open');
+  });
+
+
+
+/* Fin integration code CSS menu circulaire => source codepen.io/mahmoud-nb */
