3012bf1f2eee40e3d8b3fc9703d7a0f86bdcc505
BambaTHIAM <bambathiam88@gmail.com>
2022-08-18 23:59:09 +0200

    Remove_button

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 
-
-
-/* Début integration code CSS menu circulaire => source codepen.io/mahmoud-nb */
-
-$(".toggleMenu").on('click', function(){
-    $("#mainMenu").toggleClass('open');
-  });
-
-
-
-/* Fin integration code CSS menu circulaire => source codepen.io/mahmoud-nb */
